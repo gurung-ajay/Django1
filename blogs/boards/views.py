082c57dbf609b99e6eb42c819064d82cfcba1a6b
gurung-ajay <gooronz.ajay@gmail.com>
2024-03-31 13:25:27 +0545

    DAY-4 03/31 get practice and templates file start

--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Board
+
 # Create your views here.
 
 # logic for homepage
 def home(request):
     # Response of logic
-    return HttpResponse('Hello, World!')
+    
+    # access board objects(data) from database
+    boards = Board.objects.all()
+    #boards_names = []
+    # store board names using loop in list
+    #for board in boards:
+    #    boards_names.append(board.name)
+        
+    # joins boards_names from list as one string    
+    #response_html = '<br>'.join(boards_names)
+    
+    
+    # render home.html and send boards variable data
+    return render(request, 'home.html', {'boards' : boards})
