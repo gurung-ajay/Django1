1420fa667cd1f51fcf5c77b1d2e70e4bd2e378d8
gurung-ajay <gooronz.ajay@gmail.com>
2024-04-01 13:23:11 +0545

    DAY 5: 04/01 Test and Bootstrap

--- blogs/boards/tests.py
+++ blogs/boards/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import home
 
 # Create your tests here.
+
+# To test if home page is working properly
+class HomeTest(TestCase):
+    
+    # check succesful response from home by checking status code
+    def test_home_view_status_code(self):
+        # name='home' from urls.py for named url
+        url = reverse('home')
+        # get and store response in 'response' var
+        response = self.client.get(url)
+        # Test if Status code 200, succesful response
+        self.assertEquals(response.status_code, 200)
+        
+    # is '/' (empty sub url: 192.169.0.0/) calling the same home func as the home func in views
+    def test_home_url_resolves_home_view(self):
+        # "/" refers to the same as url path '/' or '' (empty) sub url from urls.py path
+        view = resolve("/")
+        self.assertEquals(view.func, home)
+        
