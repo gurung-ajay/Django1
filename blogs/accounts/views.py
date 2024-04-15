27cf57f449bfd96e68d15b62de4a1bd6eb086396
gurung-ajay <gooronz.ajay@gmail.com>
2024-04-15 13:09:35 +0545

    04/15 accounts test, base.html update, customize UserCreationForm, styling signup.html

--- blogs/accounts/views.py
+++ blogs/accounts/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth import login as auth_login
+
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+# instead of using predefined UserCreationForm, importing our own customized UserCreationForm from forms.py
+from .forms import SignUpForm
+
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
