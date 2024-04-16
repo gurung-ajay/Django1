741fc02554ae31a27d5cedb470d9bde442519482
gurung-ajay <gooronz.ajay@gmail.com>
2024-04-16 12:58:27 +0545

    04/16 login,logout,signup,base.html, separating test files for templates and views, templatetags

--- blogs/boards/tests/test_views.py
+++ blogs/boards/tests/test_views.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home
-from .views import board_topics, new_topic
-from .models import Board, Topic, Post
+from ..views import home
+from ..views import board_topics, new_topic
+from ..models import Board, Topic, Post
 from django.contrib.auth.models import User
 
-from .forms import NewTopicForm
+from ..forms import NewTopicForm
 
 # Create your tests here.
 
